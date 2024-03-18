2630d2dfb9f4dd5c1ab86c601e04441bf9e15185
Christopher <92882782+Krostofer@users.noreply.github.com>
2024-03-18 16:59:51 -0400

    Initial commit

---
+++ BoardFoot.swift
@@ -0,0 +1,9 @@
+
+//  BoardFoot.swift
+//
+//  Created by Christopher Di Bert
+//  Created on 2024-03-18
+//  Version 1.0
+//  Copyright (c) Christopher Di Bert. All rights reserved.
+//
+//  Change me.
